bfdeb72466c4eac70bbb28f5f4ea293e5443fb55
TheSAAADSalmon <unfish0512@gmail.com>
2026-06-21 16:34:47 +0800

    Update utils.py
    Fix embedding API key indentation

--- utils.py
+++ utils.py
@@ -86,9 +86,9 @@ def load_config(config_path: str = None) -> dict:
     if env_api_key:
         config.setdefault("dehydration", {})["api_key"] = env_api_key
 
-env_embedding_api_key = os.environ.get("OMBRE_EMBEDDING_API_KEY", "")
-if env_embedding_api_key:
-    config.setdefault("embedding", {})["api_key"] = env_embedding_api_key
+    env_embedding_api_key = os.environ.get("OMBRE_EMBEDDING_API_KEY", "")
+    if env_embedding_api_key:
+        config.setdefault("embedding", {})["api_key"] = env_embedding_api_key
 
     env_base_url = os.environ.get("OMBRE_BASE_URL", "")
     if env_base_url:
